a17a110f201ca138092ffd55fefed8040bb9ec33
teto770401-svg <teto770401@gmail.com>
2026-04-30 00:03:37 +0900

    Add: two-integer-sum - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum/submission-7.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mpp;
+        for(int i = 0; i < nums. size(); ++i){
+            if(mpp.find(target-nums[i]) != mpp.end()){
+                return {mpp[target-nums[i]], i};
+            } 
+            mpp.insert({nums[i], i});
+        }
+
+
+    }
+};
